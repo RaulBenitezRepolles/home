2777219ae3ff5948200450da78c72523afbb7353
RaulBenitezRepolles <benitezrepolles@gmail.com>
2021-10-10 05:52:53 +0200

    Add files via upload

--- crud.py
+++ crud.py
@@ -23,30 +23,30 @@ footer {visibility: hidden;}
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 #margenes
 st.markdown(f"""
-    <style>
-        .reportview-container .main .block-container{{
-            max-width: {3000}px;
-            padding-top: {1}rem;
-            padding-right: {2.5}rem;
-            padding-left: {2.5}rem;
-            padding-bottom: {1}rem;
-        }}
-    </style>
-    """,
-            unsafe_allow_html=True)
+	<style>
+		.reportview-container .main .block-container{{
+			max-width: {3000}px;
+			padding-top: {1}rem;
+			padding-right: {2.5}rem;
+			padding-left: {2.5}rem;
+			padding-bottom: {1}rem;
+		}}
+	</style>
+	""",
+			unsafe_allow_html=True)
 
 HTML_BANNER = """
-    <div style="background-color:#464e5f;padding:10px;border-radius:10px">
-    <h1 style="color:white;text-align:center;">App de Álvaro</h1>
-    </div>
-    """
+	<div style="background-color:#464e5f;padding:10px;border-radius:10px">
+	<h1 style="color:white;text-align:center;">App de Álvaro</h1>
+	</div>
+	"""
 stc.html(HTML_BANNER)
 
 password = st.sidebar.text_input("Password",'290920',type='password',key='password') 
 if password == st.secrets["password"]:
 	#botones radio horizontal
 	st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-	ListMenu = ['Compra','Calendario']
+	ListMenu = ['Compra','Tickets','Calendario']
 	ChoiceMenu = st.radio('Menú',ListMenu)
 
 	if ChoiceMenu == 'Compra':
@@ -94,6 +94,89 @@ if password == st.secrets["password"]:
 			clean_historic_df = pd.DataFrame(historic,columns=["Evento","Fecha","Activo"])
 			c3.dataframe(clean_historic_df)
 
+	if ChoiceMenu == 'Tickets':
+		import easyocr as ocr
+		import streamlit as st
+		from PIL import Image
+		import numpy as np
+		image = st.file_uploader(label= 'Ticket Mercadona')
+		@st.cache
+		def load_model():
+			reader = ocr.Reader(['es'],model_storage_directory='.')
+			return reader
+		reader = load_model()
+		if image is not None:
+			input_image = Image.open(image)
+			#Mostrar imagen
+			#st.image(input_image)
+			with st.spinner("Processing"):
+				result = reader.readtext(np.array(input_image))
+			a=0
+			j=0
+			k=[]
+			len_anterior=0
+			for i in range(len(result)):
+			    if result[i][1]=='TARJETA':
+			        a=0
+			    if a==1:
+			        if (result[i][0][0][1]>=anterior-10 and result[i][0][0][1]<=anterior+10) or len_anterior==1:
+			            k[j-1]+=[result[i][1]]
+			            anterior=result[i][0][0][1]
+			            len_anterior = len(k[j-1])
+			        else:
+			            j+=1
+			            #print(j,result[i][1])
+			            k+=[[result[i][1]]]
+			            anterior=result[i][0][0][1]
+			            len_anterior = len(k[j-1]) 
+			    if result[i][1]=='unidad':
+			        a=1
+			        anterior=result[i][0][0][1]
+			import pandas as pd
+			df = pd.DataFrame()
+			for i in range(len(k)):
+			    df.loc[i,'nombre']=k[i][0]
+			    df.loc[i,'total']=k[i][-1]
+			    if len(k[i])>=3:
+			        df.loc[i,'precio']=k[i][-2]
+			    if len(k[i])>=4:
+			        df.loc[i,'peso']=k[i][-3]
+			for i in range(df.shape[0]):
+				Articulo=df.loc[i,'nombre']
+				Total=df.loc[i,'total']
+				Precio=df.loc[i,'precio']
+				Peso=df.loc[i,'peso']
+				add_data_tickets('Mercadona',now,Articulo,Total,Precio,Peso,'1')
+			create_table_tickets()
+			result = view_active_data_tickets()
+			clean_df = pd.DataFrame(result,columns=['Supermercado','Fecha','Articulo','Total','Precio','Peso','Activo'])
+			st.dataframe(clean_df[['Supermercado','Fecha','Articulo','Total','Precio','Peso']])
+		else:			
+			create_table_tickets()
+			result = view_active_data_tickets()
+			clean_df = pd.DataFrame(result,columns=['Supermercado','Fecha','Articulo','Total','Precio','Peso','Activo'])
+			st.dataframe(clean_df[['Supermercado','Fecha','Articulo','Total','Precio','Peso']])
+
+
+		c1, c2, c3, c4, c5, c6, c7 = st.columns((1,1,1,1,1,1,1))
+		Indice = c1.number_input('Indice a modificar', min_value=0, max_value=None)
+		Supermercado = c2.text_area("Modificar supermercado")
+		Fecha = c3.text_area("Modificar Fecha")
+		Articulo = c4.text_area("Modificar Articulo")
+		Total = c5.text_area("Modificar Total")
+		Precio = c6.text_area("Modificar Precio")
+		Peso = c7.text_area("Modificar Peso")
+#		st.write(Indice,Supermercado,Fecha,Articulo,Total,Precio,Peso)
+#		st.write(view_index_tickets(Indice))
+#		if st.button("Modificar"):
+#			for Artículos in delete_by_task_name:
+#				deactivate_data_tickets()
+#			st.experimental_rerun()
+#		if st.button("Histórico"):
+#			historic = view_all_data_tickets()
+#			clean_historic_df = pd.DataFrame(historic,columns=['Supermercado','Fecha','Articulo','Total','Precio','Peso','Activo'])
+#			st.dataframe(clean_historic_df)
+
 
 
 with st.sidebar.expander('Desarrollador', expanded=False):
@@ -104,4 +187,7 @@ with st.sidebar.expander('Desarrollador', expanded=False):
 			st.experimental_rerun()
 		if st.button("Reseteo calendar"):
 			c.execute('DROP TABLE calendario')
+			st.experimental_rerun()
+		if st.button("Reseteo tickets"):
+			c.execute('DROP TABLE tickets')
 			st.experimental_rerun()
--- db_fxns.py
+++ db_fxns.py
@@ -33,7 +33,6 @@ def deactivate_data_compra(Artículo):
 
 
 def create_table_calendar():
-	#c.execute('DROP TABLE calendar')
 	c.execute('CREATE TABLE IF NOT EXISTS calendar(evento TEXT,fecha TEXT, activo NUMBER)')
 
 def add_data_calendar(evento,fecha):
@@ -59,3 +58,31 @@ def deactivate_data_calendar(Artículo):
 	c.execute('UPDATE calendar SET activo =0 WHERE evento ="{}" and activo = 1'.format(Artículo))
 	conn.commit()
 
+
+
+def create_table_tickets():
+	c.execute('CREATE TABLE IF NOT EXISTS tickets(supermercado TEXT,fecha TEXT,Articulo TEXT,total TEXT, precio TEXT, peso TEXT, activo TEXT)')
+
+def add_data_tickets(Supermercado,Fecha,Articulo,Total,Precio,Peso,Activo):
+	c.execute('INSERT INTO tickets(supermercado,fecha,Articulo,total,precio,peso,activo) VALUES (?,?,?,?,?,?,?)',(Supermercado,Fecha,Articulo,Total,Precio,Peso,Activo))
+	conn.commit()
+
+def view_active_data_tickets():
+	c.execute('SELECT * FROM tickets WHERE activo = 1')
+	data = c.fetchall()
+	return data
+
+def view_all_data_tickets():
+	c.execute('SELECT * FROM tickets')
+	data = c.fetchall()
+	return data
+
+#def view_index_tickets(INDEX):
+#	c.execute('SELECT DISTINCT 	* FROM tickets WITH(INDEX("{}"))'.format(str(INDEX)))
+#	data = c.fetchall()
+#	return data
+	
+def deactivate_data_tickets(indice,Supermercado,Fecha,Articulo,Total,Precio,Peso,Activo):
+	#c.execute('UPDATE tickets SET activo =0 WITH(INDEX("{}")) and activo = 1'.format(indice))
+	c.execute('INSERT INTO tickets(supermercado,fecha,Articulo,total,precio,peso,activo) VALUES (?,?,?,?,?,?,?)',(Supermercado,Fecha,Articulo,Total,Precio,Peso,Activo))
+	conn.commit()
