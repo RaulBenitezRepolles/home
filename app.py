62642d09afd216d1ad0ff48248d0af9684a7336e
RaulBenitezRepolles <benitezrepolles@gmail.com>
2021-10-23 21:36:04 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ import sqlite3
 from datetime import datetime
 from datetime import date
 import base64
+#import streamlit.components.v1 as components
+import hashlib
 import os
-
 
 now = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 conn = sqlite3.connect('data.db',check_same_thread=False)
 c = conn.cursor()
+create_table_users()
 
 st.set_page_config(page_title="App Álvaro")
 #ocultar menu
@@ -43,11 +45,46 @@ HTML_BANNER = """
 	"""
 stc.html(HTML_BANNER)
 
-
-user = st.sidebar.text_input("User",'Raúl',key='user') 
+user = st.sidebar.text_input("User",'',key='user') 
 password = st.sidebar.text_input("Password",'',type='password',key='password') 
-if password == st.secrets["password"]:
-	#botones radio horizontal
+with st.sidebar.expander('Desarrollador', expanded=False):
+	password_admin_input = st.text_input("Password",'',type='password',key='password_admin_input') 
+	if password_admin_input == st.secrets["password"]:
+		if st.button("Reseteo compra"):
+			try:
+				c.execute('DROP TABLE compra')
+				st.experimental_rerun()
+			except:
+				pass
+		if st.button("Reseteo calendar"):
+			try:
+				c.execute('DROP TABLE calendar')
+				st.experimental_rerun()
+			except:
+				pass
+		if st.button("Reseteo tickets"):
+			try:
+				c.execute('DROP TABLE tickets')
+				st.experimental_rerun()
+			except:
+				pass
+		st.markdown("---")
+		User_Name = st.text_input('Añadir Usuario')
+		Password_Name = st.text_input('Añadir Password')
+		if st.button("Grabar Usuario"):
+			if User_Name and Password_Name:
+				add_data_users(User_Name,Password_Name)
+			else:
+				st.write('Añadir usuario y/o contraseña')
+		if st.button("Reseteo usuarios"):
+			try:
+				c.execute('DROP TABLE usertable')
+				create_table_users()
+				st.experimental_rerun()
+			except:
+				pass
+
+if view_active_username(user, hashlib.sha256(str.encode(password)).hexdigest())[0][0] >= 1:
 	st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 	ListMenu = ['Compras','Calendario']
 	ChoiceMenu = st.radio('Menú',ListMenu)
@@ -57,53 +94,51 @@ if password == st.secrets["password"]:
 		result = view_active_data_compra()
 		clean_df = pd.DataFrame(result,columns=["Artículo","Descripción","Número","Inscripción_Fecha",'Inscripción_User','Modificación_Fecha','Modificación_User',"Activo"])
 		st.dataframe(clean_df[["Artículo","Descripción","Número"]])
-		c1, c2, c3 = st.columns((1,1,1))
+		c1, c2 = st.columns((1,1))
 		Artículo = c1.text_input("Añadir Artículo")
 		Descripción = c1.text_input("Descripción")
-		Número = c1.number_input("Número de Artículos",value=0)
-		if c1.button("Añadir",''):
-			add_data_compra(Artículo,Descripción,Número,now,user)
-			st.experimental_rerun()
 		unique_list = [i for i in view_all_task_names_compra()]
-		delete_by_task_name = c2.multiselect('Selecciona Artículos para Borrar', unique_list)
+		delete_by_task_name = c2.multiselect('Borrar Artículos', unique_list)
 		if c2.button("Borrar"):
 			for Artículos in delete_by_task_name:
 				deactivate_data_compra(Artículos[0],Artículos[1],Artículos[2],now,user)
 			st.experimental_rerun()
+
+		c1, c2 = st.columns((1,1))
+		Número = c1.number_input("Número de Artículos",value=0)
+		if c1.button("Añadir",''):
+			add_data_compra(Artículo,Descripción,Número,now,user)
+			st.experimental_rerun()
 		unique_list = [i for i in view_all_deleted_task_names_compra()]
-		delete_by_task_name = c2.multiselect('Selecciona Artículos para Reactivar', unique_list)
+		delete_by_task_name = c2.multiselect('Reactivar Artículos', unique_list)
 		if c2.button("Reactivar"):
 			for Artículos in delete_by_task_name:
 				reactivate_data_compra(Artículos[0],Artículos[1],Artículos[2],now,user)
 			st.experimental_rerun()
-		if c3.button("Histórico"):
-			historic = view_all_data_compra()
-			clean_historic_df = pd.DataFrame(historic,columns=["Artículo","Descripción","Número","Inscripción_Fecha",'Inscripción_User','Modificación_Fecha','Modificación_User',"Activo"])
-			c3.dataframe(clean_historic_df)
+		st.markdown("---")
+		c1, c2 = st.columns((1,1))
+		historic = view_all_data_compra()
+		clean_historic_df = pd.DataFrame(historic,columns=["Artículo","Descripción","Número","Inscripción_Fecha",'Inscripción_User','Modificación_Fecha','Modificación_User',"Activo"])
+		if c1.button("Histórico"):
+			c1.dataframe(clean_historic_df)
+		c2.download_button(label="Descargar CSV",data=clean_historic_df.to_csv().encode('utf-8'), file_name='Compras_historico.csv', mime='text/csv', )
 
 	if ChoiceMenu == 'Calendario':
 		create_table_calendar()
 		result = view_active_data_calendar()
 		clean_df = pd.DataFrame(result,columns=["Evento","Comentarios","Fecha","Inscripción_Fecha",'Inscripción_User','Inscripción_ID_Actualizada',"Activo"])
 		st.dataframe(clean_df[["Evento","Comentarios","Fecha"]])
-		c1, c2, c3 = st.columns((1,1,1))
+		c1, c2 = st.columns((1,1))
 		Evento = c1.text_input("Añadir Evento")
-		Comentarios = c1.text_input("Añadir Comentarios")
-
+		Comentarios = c1.text_input("Comentarios")
 		unique_list = [i for i in view_all_task_names_calendar()]
-		delete_by_task_name = c2.multiselect('Selecciona Eventos a Borrar', unique_list)
+		delete_by_task_name = c2.multiselect('Borrar Eventos', unique_list)
 		if c2.button("Borrar"):
 			for Evento in delete_by_task_name:
 				deactivate_data_calendar(Evento[0],Evento[1],Evento[2])
 			st.experimental_rerun()
-
-
-		if c3.button("Histórico"):
-			historic = view_all_data_calendar()
-			clean_historic_df = pd.DataFrame(historic,columns=["Evento","Comentarios","Fecha","Inscripción_Fecha",'Inscripción_User','Inscripción_ID_Actualizada',"Activo"])
-			c3.dataframe(clean_historic_df)
-
-		c1, c2, c3, c4 = st.columns((1,1,2,2))
+			
+		c1, c2, c3 = st.columns((1,1,2))
 		date = c1.date_input('Fecha')
 		hour = c2.time_input('Hora')
 		Fecha = str(date) +' '+ str(hour)
@@ -112,35 +147,16 @@ if password == st.secrets["password"]:
 			st.experimental_rerun()
 
 		unique_list = [i for i in view_all_deleted_task_names_calendar()]
-		delete_by_task_name = c3.multiselect('Selecciona Eventos a Reactivar', unique_list)
-		if c3.button("Reactivar Borrados"):
+		delete_by_task_name = c3.multiselect('Reactivar Eventos', unique_list)
+		if c3.button("Reactivar"):
 			for Evento in delete_by_task_name:
 				reactivate_data_calendar(Evento[0],Evento[1],Evento[2])
 			st.experimental_rerun()
+		st.markdown("---")
+		c1, c2 = st.columns((1,1))
+		historic = view_all_data_calendar()
+		clean_historic_df = pd.DataFrame(historic,columns=["Evento","Comentarios","Fecha","Inscripción_Fecha",'Inscripción_User','Inscripción_ID_Actualizada',"Activo"])
+		if c1.button("Histórico"):
+			c1.dataframe(clean_historic_df)
+		c2.download_button(label="Descargar CSV",data=clean_historic_df.to_csv().encode('utf-8'), file_name='Calendario_historico.csv', mime='text/csv', )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#with st.sidebar.expander('Desarrollador', expanded=False):
-#	password2 = st.text_input("Password",'admin',type='password',key='password2') 
-#	if password2 == 'admin':
-#		if st.button("Reseteo compra"):
-#			c.execute('DROP TABLE compra')
-#			st.experimental_rerun()
-#		if st.button("Reseteo calendar"):
-#			c.execute('DROP TABLE calendar')
-#			st.experimental_rerun()
-#		if st.button("Reseteo tickets"):
-#			c.execute('DROP TABLE tickets')
-#			st.experimental_rerun()
--- db_fxns.py
+++ db_fxns.py
@@ -1,13 +1,28 @@
 import sqlite3
+import hashlib
 conn = sqlite3.connect('data.db',check_same_thread=False)
 c = conn.cursor()
 
+
+def create_table_users():
+    c.execute('CREATE TABLE IF NOT EXISTS usertable(username TEXT,userhash TEXT)')
+
+def add_data_users(username, password):
+    c.execute('INSERT INTO usertable(username,userhash) VALUES (?,?)',(username, hashlib.sha256(str.encode(password)).hexdigest()))
+    conn.commit()
+
+def view_active_username(username, userhash):
+	c.execute('SELECT Count(username) FROM usertable WHERE username = "{}" and userhash = "{}" '.format(username,userhash))
+	data = c.fetchall()
+	return data
+
+
 def create_table_compra():
 	#c.execute('DROP TABLE compra')
-	c.execute('CREATE TABLE IF NOT EXISTS compra(Artículo TEXT, Descripción TEXT, Inscripción_Fecha TEXT, Inscripción_User TEXT, Inscripción_ID_Actualizada TEXT, Activo NUMBER)')   
+	c.execute('CREATE TABLE IF NOT EXISTS compra(Artículo TEXT, Descripción TEXT, Número NUMBER, Inscripción_Fecha TEXT, Inscripción_User TEXT, Modificación_Fecha TEXT, Modificación_User Text, Activo NUMBER)')   
 	
-def add_data_compra(Artículo,Descripción,now,user):
-	c.execute('INSERT INTO compra(Artículo,Descripción,Inscripción_Fecha,Inscripción_User,Inscripción_ID_Actualizada,Activo) VALUES (?,?,?,?,?,?)',(Artículo,Descripción,now,user,-1,1))
+def add_data_compra(Artículo,Descripción,Número,now,user):
+	c.execute('INSERT INTO compra(Artículo,Descripción,Número,Inscripción_Fecha,Inscripción_User,Modificación_Fecha,Modificación_User,Activo) VALUES (?,?,?,?,?,?,?,?)',(Artículo,Descripción,Número,now,user,now,user,1))
 	conn.commit()
 
 def view_active_data_compra():
@@ -21,21 +36,22 @@ def view_all_data_compra():
 	return data
 
 def view_all_task_names_compra():
-	c.execute('SELECT DISTINCT Artículo,Descripción FROM compra WHERE Activo = 1')
+	c.execute('SELECT DISTINCT Artículo,Descripción,Número FROM compra WHERE Activo = 1')
 	data = c.fetchall()
 	return data
 	
-def deactivate_data_compra(Artículo,Descripción):
-	c.execute('UPDATE compra SET Activo =0 WHERE Artículo ="{}" and Descripción = "{}" and Activo = 1'.format(Artículo,Descripción))
+def deactivate_data_compra(Artículo,Descripción,Número,now,user):
+	c.execute('UPDATE compra SET Activo =0 , Modificación_Fecha = "{}" , Modificación_User= "{}" WHERE Artículo ="{}" and Descripción = "{}" and Número = "{}" and Activo = 1'.format(now,user,Artículo,Descripción,Número))
 	conn.commit()
 
 def view_all_deleted_task_names_compra():
-	c.execute('SELECT DISTINCT Artículo,Descripción FROM compra WHERE Activo = 0')
+	c.execute('SELECT DISTINCT Artículo,Descripción,Número FROM compra WHERE Activo = 0 Order by Inscripción_Fecha desc')
 	data = c.fetchall()
 	return data
 	
-def reactivate_data_compra(Artículo,Descripción):
-	c.execute('UPDATE compra SET Activo =1 WHERE Artículo ="{}" and Descripción = "{}" and Activo = 0'.format(Artículo,Descripción))
+def reactivate_data_compra(Artículo,Descripción,Número,now,user):
+	c.execute('UPDATE compra SET Activo =-1 WHERE Artículo ="{}" and Descripción = "{}" and Número = "{}" and Activo = 0'.format(Artículo,Descripción,Número))
+	c.execute('INSERT INTO compra(Artículo,Descripción,Número,Inscripción_Fecha,Inscripción_User,Modificación_Fecha,Modificación_User,Activo) VALUES (?,?,?,?,?,?,?,?)',(Artículo,Descripción,Número,now,user,now,user,1))
 	conn.commit()
 
 	#c.execute('DELETE FROM compra WHERE articulo ="{}"'.format(Artículo))
@@ -49,7 +65,7 @@ def add_data_calendar(Evento,Comentarios,Fecha,now,user):
 	conn.commit()
 
 def view_active_data_calendar():
-	c.execute('SELECT * FROM calendar WHERE Activo = 1')
+	c.execute('SELECT * FROM calendar WHERE Activo = 1  Order by fecha asc')
 	data = c.fetchall()
 	return data
 
@@ -59,7 +75,7 @@ def view_all_data_calendar():
 	return data
 
 def view_all_task_names_calendar():
-	c.execute('SELECT DISTINCT evento,Comentarios,fecha FROM calendar WHERE Activo = 1')
+	c.execute('SELECT DISTINCT evento,Comentarios,fecha FROM calendar WHERE Activo = 1 Order by fecha desc')
 	data = c.fetchall()
 	return data
 	
@@ -68,7 +84,7 @@ def deactivate_data_calendar(evento,Comentarios,fecha):
 	conn.commit()
 
 def view_all_deleted_task_names_calendar():
-	c.execute('SELECT DISTINCT evento,Comentarios,fecha FROM calendar WHERE Activo = 0')
+	c.execute('SELECT DISTINCT evento,Comentarios,fecha FROM calendar WHERE Activo = 0 Order by fecha desc')
 	data = c.fetchall()
 	return data
 	
