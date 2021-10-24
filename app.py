054a6fa3aa3d9269f68bdf7d88945a6fd47ad8c1
RaulBenitezRepolles <benitezrepolles@gmail.com>
2021-10-24 03:59:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -173,12 +173,12 @@ if view_active_username(user, hashlib.sha256(str.encode(password)).hexdigest())[
 
 
 
-from plyer.utils import platform
-from plyer import notification
-
-notification.notify(
-    title='Here is the title',
-    message='Here is the message',
-    app_name='Here is the application name',
-    app_icon='path/to/the/icon.' + ('ico' if platform == 'win' else 'png')
-)
+#from plyer.utils import platform
+#from plyer import notification
+#
+#notification.notify(
+#    title='Here is the title',
+#    message='Here is the message',
+#    app_name='Here is the application name',
+#    app_icon='path/to/the/icon.' + ('ico' if platform == 'win' else 'png')
+#
